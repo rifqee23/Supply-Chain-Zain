2e4358f0480313f0f86cae421387ce3f640d9e20
rifqee23 <rifqifebrianto746@gmail.com>
2024-10-01 14:31:17 +0700

    create regis-api

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const authController = require("./auth/auth.controller");
+const app = express()
+const port = 3000
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/auth", authController);
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
