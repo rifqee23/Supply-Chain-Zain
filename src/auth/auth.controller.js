9a0b9ee14c64b5938736e4a6f316494463f6896d
rifqee23 <127317331+rifqee23@users.noreply.github.com>
2024-10-02 01:30:09 +0700

    create login-api and modify regis-api (#5)

--- src/auth/auth.controller.js
+++ src/auth/auth.controller.js
@@ -2,17 +2,18 @@ const express = require('express');
 const authService = require('./auth.service');
 const router = express.Router();
 
-router.post("/register", async (req, res, next) => {
-    const {username, email, password} = req.body;
+router.post('/register', async (req, res, next) => {
+    const {username, email, password, role} = req.body;
 
     try {
-        const newUser = await authService.register(username, email, password);
+        const newUser = await authService.register(username, email, password, role);
         res.status(201).json({
             data: {
                 username: newUser.username,
                 email: newUser.email,
+                role: newUser.role
             },
-            message: "Register successfully",
+            message: 'Register successfully',
         })
     } catch (e) {
         res.status(400).json({
@@ -20,5 +21,21 @@ router.post("/register", async (req, res, next) => {
         })
     }
 });
+
+router.post("/login", async (req, res, next) => {
+    const {username, password} = req.body;
+    try {
+        const user = await authService.login(username, password);
+        res.status(200).json({
+            data: {
+                username: user.username,
+                role: user.role
+            },
+            message: 'Login successfully',
+        })
+    }catch (err) {
+        res.status(400).json({error: err.message})
+    }
+})
 
 module.exports = router;
