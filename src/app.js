ccce4cfd2f43b95e731876d31923f98f532947bc
rifqee23 <127317331+rifqee23@users.noreply.github.com>
2024-11-29 06:32:28 +0700

    chore(deploy): initial deployment to production (#17)

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = 3000;
-const dotenv = require('dotenv').config();
-const adminAuthorization = require('./middleware/adminAuthorization');
-const cors = require('cors');
-
+// const port = 3000;
+const dotenv = require("dotenv");
+dotenv.config();
+const adminAuthorization = require("./middleware/adminAuthorization");
+const cors = require("cors");
 
 // Middleware
 app.use(express.json());
@@ -12,15 +12,15 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 // Root route
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 // Import controllers
 const authController = require("./auth/auth.controller");
 const productController = require("./product/product.controller");
 const userController = require("./user/user.controller");
-const orderController = require('./order/order.controller');
+const orderController = require("./order/order.controller");
 
 // Routes
 app.use("/api/auth", authController);
@@ -29,6 +29,6 @@ app.use("/api/users", adminAuthorization, userController);
 app.use("/api/orders", orderController);
 
 // Start the server
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`);
+// });
